3cc13a7afe445331f4971b565f71873dd1694241
D1N1S1O <matosdinis232@gmail.com>
2026-03-03 22:21:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,21 @@ Session(app)
 
 @app.route("/", methods=["GET"])
 def index():
+
     if "mes" not in session:
         session["mes"] = 1
         session["prev"] = 0
         session["current"] = 1
-    return render_template("index.html", mes=session["mes"], coelhos=session["current"])
+    #coelhos_visiveis = min(session["current"], 55)
+    #limitado = session["current"] > 55
+    coelhos_visiveis = session["current"]
+
+    return render_template(
+        "index.html",
+        mes=session["mes"],
+        coelhos=session["current"],
+        coelhos_visiveis=coelhos_visiveis,
+    )
 
 @app.route("/avancar", methods=["POST"])
 def avancar():
@@ -22,7 +32,16 @@ def avancar():
     session["prev"] = current
     session["current"] = next_value
     session["mes"] = session.get("mes", 1) + 1
-    return render_template("index.html", mes=session["mes"], coelhos=session["current"])
+    #coelhos_visiveis = min(session["current"], 55)
+    #limitado = session["current"] > 55
+    coelhos_visiveis = session["current"]
+
+    return render_template(
+        "index.html",
+        mes=session["mes"],
+        coelhos=session["current"],
+        coelhos_visiveis=coelhos_visiveis,
+    )
 
 
 @app.route("/recuar", methods=["POST"])
@@ -33,14 +52,33 @@ def recuar():
         session["prev"] = current - prev
         session["current"] = prev
         session["mes"] -= 1
-    return render_template("index.html", mes=session["mes"], coelhos=session["current"])
+        #coelhos_visiveis = min(session["current"], 55)
+        #limitado = session["current"] > 55
+        coelhos_visiveis = session["current"]
+        limitado = False;
+
+    return render_template(
+        "index.html",
+        mes=session["mes"],
+        coelhos=session["current"],
+        coelhos_visiveis=coelhos_visiveis,
+    )
 
 @app.route("/voltar", methods=["POST"])
 def voltar():
     session["mes"] = 1
     session["prev"] = 0
     session["current"] = 1
-    return render_template("index.html", mes=session["mes"], coelhos=session["current"])
+    #coelhos_visiveis = min(session["current"], 55) para limitar os coelhos visiveis
+    #limitado = session["current"] > 55
+    coelhos_visiveis = session["current"]
+
+    return render_template(
+        "index.html",
+        mes=session["mes"],
+        coelhos=session["current"],
+        coelhos_visiveis=coelhos_visiveis,
+    )
 
 
 
